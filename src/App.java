b77e15dbc7c4472e357bddef5c5f154fded07ae5
André Silveira <114875096+Dedecow@users.noreply.github.com>
2023-09-23 12:45:13 -0300

    classe pessoaFisica

--- src/App.java
+++ src/App.java
@@ -3,3 +3,26 @@ public class App {
         System.out.println("Hello, World!");
     }
 }
+public class PessoaFisica {
+    private String nome;
+    private String cpf;
+
+    public PessoaFisica(String nome, String cpf) {
+        setCPF(cpf); // Chamando o setter para validação do CPF
+        this.nome = nome;
+    }
+
+    public void setCPF(String cpf) {
+        if (validarCPF(cpf)) {
+            this.cpf = cpf;
+        } else {
+            throw new IllegalArgumentException("CPF inválido");
+        }
+    }
+
+    public boolean validarCPF(String cpf) {
+        return true; // Substitua por sua lógica de validação real
+    }
+
+}
+
