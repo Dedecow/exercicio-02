35bec082e2633b8cbfdd7de76063187858aeefab
Andre <andrehbahia@gmail.com>
2023-10-14 21:58:19 -0300

    Semana01-exercicio-05- ok

---
+++ src/br/org/senai/ava/modulo02/semana01/exercicio05/IdadeCalculada.java
@@ -0,0 +1,18 @@
+package br.org.senai.ava.modulo02.semana01.exercicio05;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class IdadeCalculada {
+    private LocalDate dataDeNascimento;
+
+    public IdadeCalculada(LocalDate dataDeNascimento) {
+        this.dataDeNascimento = dataDeNascimento;
+    }
+
+    public int calcularIdade() {
+        LocalDate dataAtual = LocalDate.now();
+        Period period = Period.between(dataDeNascimento, dataAtual);
+        return period.getYears();
+    }
+}
